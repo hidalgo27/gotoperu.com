6f3fbf512ef67f4290ee6b4850f82c43f8ee921e
Hidalgo <hidalgochpnce@gmail.com>
2018-09-28 12:10:00 -0500

    Se integro tours con fare

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -140,20 +140,20 @@ class HomeController extends Controller
 
     public function peru_tours()
     {
-        SEOMeta::setTitle('Machu Picchu Tour Packages | Machu Picchu Vacation Packages | Machu Picchu Deals | Peru Honeymoon Travel Packages');
+        SEOMeta::setTitle('Machu Picchu Tours | Peru Tours');
         SEOMeta::setDescription('Want to travel to Peru? GoToPeru offers a variety travel packages all over Peru. Call one of our offices today to start planning your Machu Picchu trip!');
         SEOMeta::setCanonical('https://gotoperu.com/packages');
-        SEOMeta::addKeyword(['Machu Picchu Tour Packages', 'Machu Picchu Packages', 'Machu Picchu Vacation Packages', 'Machu Picchu Deals', 'Peru Honeymoon Travel Packages']);
+        SEOMeta::addKeyword(['Machu Picchu Tours', 'Machu Picchu Tours', 'Machu Picchu Vacation Tours', 'Machu Picchu Deals', 'Peru Tours']);
 
         OpenGraph::setDescription('Want to travel to Peru? GoToPeru offers a variety travel packages all over Peru. Call one of our offices today to start planning your Machu Picchu trip!');
-        OpenGraph::setTitle('Machu Picchu Tour Packages | Machu Picchu Vacation Packages | Machu Picchu Deals | Peru Honeymoon Travel Packages');
+        OpenGraph::setTitle('Machu Picchu Tours | Peru Tours');
         OpenGraph::setUrl('https://gotoperu.com/packages');
         OpenGraph::addImages(['url'=>'https://gotoperu.com/images/banners/cusco.jpg']);
         OpenGraph::setSiteName('goto-peru');
         OpenGraph::addProperty('type', 'website');
 
         \Twitter::setType('summary');
-        \Twitter::setTitle('Travel Packages to Peru | Peru Vacations | Machu Picchu Travel');
+        \Twitter::setTitle('Machu Picchu Tours | Peru Tours');
         \Twitter::setSite('@GOTOPERUCOM');
         \Twitter::addImage('https://gotoperu.com/images/banners/cusco.jpg');
 
