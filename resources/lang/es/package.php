07dd4371eb9ef483aafd479a93a2a95bf43251d3
Hidalgo <hidalgochpnce@gmail.com>
2019-06-21 12:50:20 -0500

    olark

--- resources/lang/es/package.php
+++ resources/lang/es/package.php
@@ -2,29 +2,30 @@
 return [
     'tours_packages' => 'PAQUETES TURISTICOS',
     'h1' => 'Viajes a Perú <span class="text-g-green">PAQUETES DESTACADOS</span>',
-    'h1_p' => 'Nuestros itinerarios más populares <b>En Perú y Sur America</b>, Estos <strong>packages</strong> pueden ser usados como referencia para perzonalizar tu viaje. At <strong>GOTOPERU</strong> Nos especializamos en la elaboración de experiencias personalizadas basadas en sus preferencias; winvitamos a revisar estos programas para tener una idea de los destinos más importantes, por ejemplo, MachuPicchu, Lake Titicaca, Nazca y el Amazonas e incluso viajes y aventuras en distintos países de la región <strong>Brasil, Ecuador , Argentina.</strong>',
-    'days' => 'dias',
+    'h1_p' => 'Nuestros paquetes más populares <b>En Perú y América del Sur</b>, Estos <strong>paquetes</strong> pueden ser usados como referencia para personalizar tu viaje. At <strong>GOTOPERU</strong> Nos especializamos en la elaboración de experiencias personalizadas basadas en sus preferencias; invitamos a revisar estos programas para tener una idea de los destinos más importantes, por ejemplo, Machu Picchu, Lake Titicaca, Nazca y el Amazonas e incluso viajes y aventuras en distintos países de la región <strong>Brasil, Ecuador , Argentina.</strong>',
+    'days' => 'días',
     'inquire' => 'Pedir cotización',
 
-    'days_tours' => 'Dias de excursiones',
-    'h1_p_alert' => 'E incluso viajes y aventuras en distintos países de la región. Brasil, Ecuador,Argentina.',
+    'days_tours' => 'Días de excursiones',
+    'h1_p_alert' => 'E incluso viajes y aventuras en distintos países de la región. Brasil, Ecuador, Argentina.',
 
     'peru_travel_packages_category' => 'Paquetes de Viaje a Perú por categoría',
 
 
     'peru_destinations' => 'Destinos en Perú',
     'h1_destinations' => 'DESTINOS EN <span class="text-g-green">PERÚ</span>',
-    'h1_destinations_p' => 'Le ofrecemos una experiencia de viaje auténtica y mágica en Perú, lo invitamos a explorar los numerosos paquetes de viaje totalmente personalizados que tenemos y que se adaptan a cada presupuesto. Permítanos planear su inolvidable viaje a nuestro majestuoso Machupicchu y al resto de nuestra patria ... ¡Perú!',
+    'h1_destinations_p' => 'Le ofrecemos una experiencia de viaje auténtica y mágica en Perú, lo invitamos a explorar los numerosos paquetes de viaje totalmente personalizados que tenemos y que se adaptan a cada presupuesto. Permítanos planear su inolvidable viaje a nuestro majestuoso Machu Picchu y al resto de nuestra patria... ¡Perú!',
     'h1_destinations_alert' => 'Trabajaremos de acuerdo con su agenda y sus intereses de viaje para construir juntos los planes de viaje más exclusivos.',
     'destinations' => 'Destinos',
     'tours' => 'Tours',
-    'h5_destinations_alert' => 'Operadores Locales en Perú: Los mejores guias de la región,  encantadores hoteles, excursiones unicas, y amistosos representantes. <span class="d-block font-weight-bold">#gotoperu #TuconexiónconPerú</span>',
-    'location' => 'Locación',
+    'h5_destinations_alert' => 'Operadores Locales en Perú: Los mejores guias de la región,  encantadores hoteles, excursiones únicas, y amistosos representantes. <span class="d-block font-weight-bold">#gotoperu #TuconexionconPeru</span>',
+    'location' => 'Ubicación',
     'history' => 'Historia',
     'geography' => 'Geografía',
     'hotels' => 'Hoteles',
     'services' => 'Servicios',
-    'current_weather' => 'Clima acutal',
+    'current_weather' => 'Clima actual',
     'cusco_weather' => 'CLIMA EN CUSCO',
     'quick_facts' => 'DATOS DE INTERES',
 ];
+
