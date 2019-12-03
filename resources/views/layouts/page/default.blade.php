ca9acf6f6db02f421dc7d96e5436bcd36a29ef92
Hidalgo Mac <hidalgoponce@icloud.com>
2019-12-03 14:45:41 -0500

    Codigo pixel adrol insert

--- resources/views/layouts/page/default.blade.php
+++ resources/views/layouts/page/default.blade.php
@@ -136,6 +136,26 @@
 <script src="{{asset("js/app.js")}}"></script>
 <script src="{{asset("js/plugins.js")}}"></script>
 @if ($locale == "es")
+    <script type="text/javascript">
+        adroll_adv_id = "EQ7PNURSN5GQXIR4E3NPZ7";
+        adroll_pix_id = "ZL44EX2JPFDJVGYRVMILCQ";
+
+        (function () {
+            var _onload = function(){
+                if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+                if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+                var scr = document.createElement("script");
+                var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+                scr.setAttribute('async', 'true');
+                scr.type = "text/javascript";
+                scr.src = host + "/j/roundtrip.js";
+                ((document.getElementsByTagName('head') || [null])[0] ||
+                    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+            };
+            if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+            else {window.attachEvent('onload', _onload)}
+        }());
+    </script>
     <script src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/d1b447b4-892c-4562-a6dc-536d0eb23944-loader.js" ></script>
 @endif
 <script>
